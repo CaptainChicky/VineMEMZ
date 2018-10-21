9f053d019707bd4063ebfe3a5ef02c9eea06c3f1
Pr0j3c1X <42329623+Pr0j3c1X@users.noreply.github.com>
2018-10-21 20:52:27 +0800

    Create Readme.mk

---
+++ Data/Readme.mk
@@ -0,0 +1,13 @@
+1.bin=1.bmp
+2.bin=2.bmp
+3.bin=Installer.exe
+4.bin=4.mp3
+5.bin=5.mp3
+6.bin=6.mp3
+7.bin=7.mp3
+8.bin=8.mp3
+9.bin=Sword.ani
+10.bin=10.mp3
+13.bin=13.bmp
+14.bin=tree.exe
+15.bin=15.mp3
